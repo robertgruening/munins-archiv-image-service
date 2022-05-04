9c0dd49ae8a30ae2cb42b3686072687a4f97da16
Robert Grüning <r.gruening@web.de>
2022-05-04 10:12:52 +0200

    in Funktion gefasst

--- image.php
+++ image.php
@@ -1,33 +1,44 @@
 <?php
 
-echo "init\n";
+error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 
-$isToResize = false;
-$resizeWidth = 0;
-$orientation = "landscape"; // landscape | orientation
-
-if (isset($_POST["resizeWidth"])) {
-    $isToResize = true;
-    $resizeWidth = $_POST["resizeWidth"];
-    echo "option: resizeWidth\n";
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    Post();
 }
 
-if (isset($_POST["orientation"])) {
-    $orientation = $_POST["orientation"];
-    echo "option: orientation\n";
+function Post()
+{
+	echo "Post()\n";
+
+	$isToResize = false;
+	$resizeWidth = 0;
+	$orientation = "landscape"; // landscape | orientation
+
+	if (isset($_POST["resizeWidth"])) {
+		$isToResize = true;
+		$resizeWidth = $_POST["resizeWidth"];
+		echo "option: resizeWidth\n";
+	}
+
+	if (isset($_POST["orientation"])) {
+		$orientation = $_POST["orientation"];
+		echo "option: orientation\n";
+	}
+
+	echo "tmp_name: ".$_FILES["file"]["tmp_name"]."\n";
+	echo "name: ".$_FILES["file"]["name"]."\n";
+	echo "size: ".$_FILES["file"]["size"]."\n";
+
+
+	$target_dir = "uploads/";
+	$target_file = $target_dir.basename($_FILES["file"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	$check = getimagesize($_FILES["file"]["tmp_name"]);
+
+	echo "mime: ".$check["mime"]."\n";
+
+	echo "done";
 }
-
-echo "tmp_name: ".$_FILES["file"]["tmp_name"]."\n";
-echo "name: ".$_FILES["file"]["name"]."\n";
-echo "size: ".$_FILES["file"]["size"]."\n";
-
-
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["file"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$check = getimagesize($_FILES["file"]["tmp_name"]);
-
-echo "mime: ".$check["mime"]."\n";
-
-echo "done";
